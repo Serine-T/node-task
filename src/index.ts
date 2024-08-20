ed1764946e4a5f7c900c060cb9597661a37d80f3
Serine-t <serinetovmasyan98@gmail.com>
2024-08-20 22:39:41 +0400

    finalise

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,24 @@
 import { readdir, stat } from 'fs/promises';
 import { join } from 'path';
 
-const dirNames=  async  (dirPath: string, level: number = 0): Promise<void> => {
+const dirNames = async (dirPath: string, level: number = 0): Promise<void> => {
   try {
     const items = await readdir(dirPath);
     
     for (const item of items) {
-      // console.log('dirPath', dirPath);
       const fullPath = join(dirPath, item);
       const stats = await stat(fullPath);
       
-      // console.log(`${stats.isDirectory() ? '' : '  '} ${item}`);
+      console.log(`${'|  '.repeat(level)}${item}`);
       
       if (stats.isDirectory()) {
         await dirNames(fullPath, level + 1);
       }
     }
   } catch (e) {
-    console.error(e);
-  }
+    console.error(e);  }
 }
 
+const srcPath = __dirname;
 
-const startPath = process.argv[1];
-console.log('process.argv', process.argv[1])
-console.log('startPath', startPath)
-
-dirNames(startPath);
+dirNames(srcPath);
